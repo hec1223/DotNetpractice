2f672a71c336fe36fc7c967180eaa2a62a5062ec
hgallegos <hector.gallegos@itron.com>
2019-09-24 09:52:54 -0500

    Initial check-in of module DotNetpractice

---
+++ DotNetpractice/Fizzbuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice
+{
+    class Fizzbuzz
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                fizzBuzzCheck(i);
+            }
+        }
+
+        //static void fizzBuzzCheck(int number)
+        //{
+        //    string output = null;
+        //    if (number % 3 == 0)
+        //    {
+        //        output += "Fizz";
+        //    }
+        //    if (number % 5 == 0)
+        //    {
+        //        output += "Buzz";
+        //    }
+        //    Console.WriteLine(output ?? number.ToString());
+        //}
+
+        static void fizzBuzzCheck(int number)
+        {
+
+        }
+    }
+}
